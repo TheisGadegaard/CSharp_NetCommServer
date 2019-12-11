340bc01b5dcd2223bdd7b9715229498d44f67e8d
TheisGadegaard <theis.gadegaard@gmail.com>
2019-12-11 14:20:54 +0100

    Removed some old code from Kalaha.cs

--- NetCommServer/Kalaha.cs
+++ NetCommServer/Kalaha.cs
@@ -124,15 +124,6 @@ namespace NetCommServer
             }
 
             return returnValue;
-
-          /*  return "KALAHA " + activePlayer + " " + winner + " " + board[0] + " " + board[1] + 
-                                            " " + board[2] + " " + board[3] + 
-                                            " " + board[4] + " " + board[5] + 
-                                            " " + board[6] + " " + board[7] + 
-                                            " " + board[8] + " " + board[9] + 
-                                            " " + board[10] + " " + board[11] + 
-                                            " " + board[12] + " " + board[13];
-          */
         }
 
 
